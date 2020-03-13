c6845b456f4397a57f141de419f8e93273660011
Mirko Rodriguez <mirko.rodriguezm@gmail.com>
2020-03-12 20:27:51 -0500

    Updating script files for testing service via gRPC.

--- environment/PROD/scripts/test-service.py
+++ environment/PROD/scripts/test-service.py
@@ -1,41 +1,60 @@
-import argparse
-import json
+#!/usr/bin/env python
+
 import numpy as np
-import requests
-from keras.applications import inception_v3, vgg16, resnet50, mobilenet
-from keras.applications.imagenet_utils import decode_predictions
+import tensorflow as tf
+from grpc.beta import implementations
+
+import predict_pb2
+import prediction_service_pb2
+
 from keras.preprocessing import image
 
-# Argumentos
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="La ruta de la imagen es necesaria.")
-ap.add_argument("-m", "--model", required=True, help="El nombre del modelo es necesario.")
-args = vars(ap.parse_args())
-
-image_path = args['image']
-model_name = args['model']
-
-print("\nModel:",model_name)
-print("Image:",image_path)
+tf.app.flags.DEFINE_string("host", "127.0.0.1", "gRPC server host")
+tf.app.flags.DEFINE_integer("port", 8500, "gRPC server port")
+tf.app.flags.DEFINE_string("model_name", "flowers_model_tl-full", "TensorFlow model name")
+tf.app.flags.DEFINE_integer("model_version", 1, "TensorFlow model version")
+tf.app.flags.DEFINE_float("request_timeout", 10.0, "Timeout of gRPC request")
+FLAGS = tf.app.flags.FLAGS
 
 
-# Preprocesar imagen
-img = image.img_to_array(image.load_img(image_path, target_size=(224, 224)))
-img = img / 255.
-img = img.astype('float32')
+def main():
+  host = FLAGS.host
+  port = FLAGS.port
+  model_name = FLAGS.model_name
+  model_version = FLAGS.model_version
+  request_timeout = FLAGS.request_timeout
 
-payload = {"instances": [{'input_image': img.tolist()}]}
+  image_filepaths = ["test-image.jpg"]
 
-# URI
-uri = ''.join(['http://127.0.0.1:9000','/v1/models/',model_name,':predict'])
-print("URI:",uri)
+  for index, image_filepath in enumerate(image_filepaths):
+    image_ndarray = image.img_to_array(image.load_img(image_filepaths[0], target_size=(224, 224)))
+    image_ndarray = image_ndarray / 255.
 
-# Request al modelo desplegado en TensorFlow Serving
-r = requests.post(uri, json=payload)
-pred = json.loads(r.content.decode('utf-8'))
+  # Create gRPC client and request
+  channel = implementations.insecure_channel(host, port)
+  stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+  request = predict_pb2.PredictRequest()
+  request.model_spec.name = model_name
+  request.model_spec.version.value = model_version
+  request.inputs['input_image'].CopyFrom(tf.contrib.util.make_tensor_proto(image_ndarray, shape=[1] + list(image_ndarray.shape)))
 
-# Decodificando decoder util
-predictions = decode_predictions(np.array(pred['predictions']),top=1)
-print("Predictions:\n",predictions)
-print("Class:",predictions[0][0][1])
-print("Score",predictions[0][0][2])
+  # Send request
+  result = str(stub.Predict(request, request_timeout))
+  mylist = result.split('\n')[-8:-3]
+  finallist = []
+  for element in mylist:
+      element = element.split(':')[1]
+      finallist.append(float("{:.6f}".format(float(element))))
+
+  index = finallist.index(max(finallist))
+  CLASSES = ['Daisy', 'Dandelion', 'Rosa', 'Girasol', 'Tulipán']
+
+  ClassPred = CLASSES[index]
+  ClassProb = finallist[index]
+
+  print(finallist)
+  print(ClassPred)
+  print(ClassProb)
+
+if __name__ == '__main__':
+  main()
